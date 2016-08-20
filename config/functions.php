2f6c0c8bf048eb50934ba6e02f1bbdd03d121bad
Alex Inman <alexinmann@gmail.com>
2016-08-20 13:45:05 -0300

    Change in some environment variables

--- config/functions.php
+++ config/functions.php
@@ -440,22 +440,26 @@ function cpr($var, $limit=2, $tab=""){
 }
 
 function email($to, $subject, $message, $from){
-	try{
-		$sendgrid_username = $_ENV['SENDGRID_USERNAME'];
-		$sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
-		$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
-		$email = new SendGrid\Email();
-		$email->addTo($to)->
-				setFrom($from)->
-				setSubject($subject)->
-				setText($message)->
-				addHeader('X-Sent-Using', 'SendGrid-API')->
-				addHeader('X-Transport', 'web');
-		$response = $sendgrid->send($email);
-		$responseBody = $response->getBody();
-		return isset($responseBody) && $responseBody['message'] == 'success';
-	}catch(Exception $e){
-		error_log($e->getMessage());
+	if(HEROKU && PRODUCTION){
+		try{
+			$sendgrid_username = $_ENV['SENDGRID_USERNAME'];
+			$sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
+			$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
+			$email = new SendGrid\Email();
+			$email->addTo($to)->
+					setFrom($from)->
+					setSubject($subject)->
+					setText($message)->
+					addHeader('X-Sent-Using', 'SendGrid-API')->
+					addHeader('X-Transport', 'web');
+			$response = $sendgrid->send($email);
+			$responseBody = $response->getBody();
+			return isset($responseBody) && $responseBody['message'] == 'success';
+		}catch(Exception $e){
+			error_log($e->getMessage());
+			return false;
+		}
+	}else{
 		return false;
 	}
 }
