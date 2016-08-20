c85858890cdd913667563b4440e996d222a9b9db
Alex Inman <alexinmann@gmail.com>
2016-08-20 14:07:21 -0300

    Renamed SQL for #60 to today's date

--- sql/2016-08-20.sql
+++ sql/2016-08-20.sql
@@ -9,3 +9,4 @@ create table war_assignment(
 	foreign key(war_id, player_id) references war_player(war_id, player_id) on delete cascade,
 	foreign key(war_id, assigned_player_id) references war_player(war_id, player_id) on delete cascade
 );
+
