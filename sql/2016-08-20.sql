169caaf4f757d180e9e174fb820ba0522de03af7
Alex Inman <alexinmann@gmail.com>
2016-08-24 20:40:08 -0300

    Fixed p_player_update_bulk to update player's date_modified

--- sql/2016-08-20.sql
+++ sql/2016-08-20.sql
@@ -11,3 +11,28 @@ create table war_assignment(
 );
 
 alter table war add status int default 0;
+
+drop procedure if exists p_player_update_bulk;
+delimiter //
+create procedure p_player_update_bulk(varId int, varRank varchar(2), varLevel int, varTrophies int, varDonations int, varReceived int, varLeagueUrl varchar(200), varDate datetime, varName varchar(50))
+begin
+    update player set name=varName, level=varLevel, trophies=varTrophies, donations=varDonations, received=varReceived, league_url=varLeagueUrl, date_modified=varDate where id = varId;
+    update clan_member set rank=varRank where player_id = varId and rank != 5;
+    if (varLevel <> (select stat_amount from player_stats where player_id = varId and stat_type = 'LV' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'LV' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'LV', varLevel);
+    end if;
+    if (varTrophies <> (select stat_amount from player_stats where player_id = varId and stat_type = 'TR' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'TR' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'TR', varTrophies);
+    end if;
+    if (varDonations <> (select stat_amount from player_stats where player_id = varId and stat_type = 'DO' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'DO' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'DO', varDonations);
+    end if;
+    if (varReceived <> (select stat_amount from player_stats where player_id = varId and stat_type = 'RE' order by date_recorded desc limit 1)
+        or not exists (select * from player_stats where player_id = varId and stat_type = 'RE' limit 1))
+        then insert into player_stats (player_id, date_recorded, stat_type, stat_amount) values (varId, varDate, 'RE', varReceived);
+    end if;
+end //
+delimiter ;
